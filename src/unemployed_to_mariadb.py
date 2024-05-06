2596fca5b8b3f7979ea27e8905c0d3ec13e2a0d8
jedsaxon <jed-sax@proton.me>
2024-05-06 10:04:34 +1000

    Python script cleaned up with percentages for each file parsed

--- src/unemployed_to_mariadb.py
+++ src/unemployed_to_mariadb.py
@@ -1,4 +1,6 @@
 import mariadb
+from os.path import join as path_join 
+from math import floor
 import sys
 import datetime
 from decimal import Decimal
@@ -13,7 +15,6 @@ try:
             password="password",
             host="localhost",
             port=3306,
-            database="datavis"
             )
 except mariadb.Error as e:
     print("Mariadb error: " + e)
@@ -21,7 +22,13 @@ except mariadb.Error as e:
 
 cur = connection.cursor()
 
+dataset_dir = input("Enter dataset location: ") 
+dataset_unemployed = path_join(dataset_dir, "unemployed.csv")
+
 connection.autocommit = False
+cur.execute("drop database if exists datavis")
+cur.execute("create database datavis");
+cur.execute("use datavis");
 cur.execute("drop table if exists main")
 cur.execute("drop table if exists reference_area")
 cur.execute("drop table if exists obs_status")
@@ -31,13 +38,16 @@ cur.execute("create table obs_status(id char(1) primary key, name varchar(100) n
 cur.execute("create table main(id int primary key auto_increment, ref_area_code varchar(12) not null, obs_status_id char(1) not null, time_period date not null, obs_value decimal(16, 8), foreign key(ref_area_code) references reference_area(code), foreign key(obs_status_id) references obs_status(id))");
 connection.commit()
 
-unemployed_dataset = "../../../cos30045-datasets/unemployed.csv"
 ref_area_codes = {}
 obs_status_codes = {}
-with open(unemployed_dataset, "r") as f:
+
+# --- UNEMPLOYED DATASET ---
+with open(dataset_unemployed, "r") as f:
     lines = f.readlines()
     line_c = len(lines)
     index = 0;
+
+    print("Parsing "+str(line_c)+" records in unemployed dataset")
     for row in lines:
         if index == 0:
             index += 1
@@ -49,7 +59,7 @@ with open(unemployed_dataset, "r") as f:
         try:
             time_period = time_preiod_mariadb_format(records[2])
         except ValueError as e:
-            print("Could not convert date %s into yyyy-mm-dd for record %s/%s", time_period, str(index), str(line_c))
+            print("Could not convert date "+records[2]+" into yyyy-mm-dd for record "+str(index)+"/"+str(line_c))
             continue
         obs_value = Decimal(records[3])
         obs_status_id = records[4]
@@ -64,9 +74,13 @@ with open(unemployed_dataset, "r") as f:
             obs_status_codes[obs_status_id] = obs_status
 
         cur.execute("insert into main(ref_area_code, obs_status_id, time_period, obs_value) values(?, ?, ?, ?)", (ref_area_code, obs_status_id, (time_period.strftime("%Y-%m-%d")), str(obs_value)))
-        print("Inserted record %s/%s\n", str(index), str(line_c))
+        # print("Inserted record" + str(index) + "/" + str(line_c))
         
+        if floor(index % (line_c / 10)) == 0: 
+            percentage = index / line_c;
+            print(" - "+str(floor(percentage * 100))+"%")
         index+=1;
+    print(" - 100%")
 
 connection.commit()
 
