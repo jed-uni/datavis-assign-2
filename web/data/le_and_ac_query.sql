861f66e9121c76e2dc7561eb1099244959d99d33
jedsaxon <jed-sax@proton.me>
2024-05-29 13:31:08 +1000

    Fix query with reference area name

--- web/data/le_and_ac_query.sql
+++ web/data/le_and_ac_query.sql
@@ -1,3 +1,5 @@
-SELECT ac.time_period, ac.ref_area_code, ac.obs_value as 'Alcohol Consumption', le.obs_value as `Life Expectency`
+SELECT ac.time_period, reference_area.name, ac.ref_area_code, ac.obs_value as 'Alcohol Consumption', le.obs_value as `Life Expectency`
 FROM alcohol_consumption ac
-JOIN life_expectency le ON ac.ref_area_code = le.ref_area_code AND ac.time_period = le.time_period;
+         JOIN life_expectency le ON ac.ref_area_code = le.ref_area_code AND ac.time_period = le.time_period
+         JOIN reference_area ON ac.ref_area_code = reference_area.code
+
