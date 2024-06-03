cad0e38d64b17a3d163e2648271a96766f59a2f7
jedsaxon <jed-sax@proton.me>
2024-06-03 12:30:02 +1000

    Update data on top 10 alcohol consumers to use only 2019 data

--- data/top_10_alcohol_consumers.sql
+++ data/top_10_alcohol_consumers.sql
@@ -1,7 +1,8 @@
 SELECT
-    reference_area.name,
-    AVG(alcohol_consumption.obs_value) AS 'average_alcohol_consumption',
-    AVG(life_expectency.obs_value) AS 'average_life_expectancy'
+    reference_area.name AS 'ref_area_name',
+    reference_area.code AS 'ref_area_code',
+    alcohol_consumption.obs_value AS 'average_alcohol_consumption',
+    life_expectency.obs_value AS 'average_life_expectancy'
 FROM
     reference_area
 CROSS JOIN
@@ -10,11 +11,8 @@ LEFT JOIN
     alcohol_consumption ON reference_area.code = alcohol_consumption.ref_area_code AND times.time_period = alcohol_consumption.time_period
 LEFT JOIN
     life_expectency ON reference_area.code = life_expectency.ref_area_code AND times.time_period = life_expectency.time_period
-GROUP BY
-    reference_area.code
-HAVING
-    average_alcohol_consumption IS NOT NULL
-   AND average_life_expectancy IS NOT NULL
+WHERE
+    times.time_period = 2019
 ORDER BY
-    average_alcohol_consumption DESC
+    alcohol_consumption.obs_value DESC
 LIMIT 10
